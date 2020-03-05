f769c30aa4209b14cf66d5a0337e6dc8be9ac11d
Aditya Dhara <ocamlmycaml@github.com>
2020-03-04 21:52:50 -0500

    Clarify note on where to add deterrent logic

--- deterrent-service/deterrent_manager.py
+++ deterrent-service/deterrent_manager.py
@@ -1,8 +1,12 @@
 
+# note: we keep these two functions separate since our logic to decide
+# which deterrent to deploy might get pretty complicated.
 
 def choose_deterrent(detected_animals):
     """Here we choose deterrent to deploy"""
+    # TODO: choose a deterrent using a map maybe?
 
 
 def deploy_deterrent(deploy_deterrent):
     """Here we deploy a deterrent"""
+    # TODO: deploy a deterrent. Here we call different functions for each deterrent
