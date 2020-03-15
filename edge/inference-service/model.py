43dc0917aa52628bc88b0e6c07bb080891a88cfe
Catherine Cao <c.weiwei.cao@gmail.com>
2020-03-15 21:54:15 +0000

    moved model file to edge

--- edge/inference-service/model.py
+++ edge/inference-service/model.py
@@ -1,3 +1,4 @@
+from tensorflow.python.keras.models import load_model as keras_load_model
 
 class StupidModel(object):
     def __call__(self, image):
@@ -6,8 +7,8 @@ class StupidModel(object):
 
 def load_model():
     """Here lies logic to load the model"""
-    # TODO: load an actual model
-    model = load_model('weights/combined/combined_model.h5')
+    # load model from h5 file
+    model = keras_load_model('model/combined_model.h5')
     return model
 
 
@@ -26,9 +27,11 @@ def infer(model, image):
     
     classes = ['skunk','fox','rodent','dog','squirrel','cat','rabbit','bird','cow','bobcat','deer','raccoon','coyote','opossum']
     classes_dict_lookup = dict(zip(range(15), classes+'other'))
-
+    
+    # run inference
     predicted_id = model.predict(image)
     predicted_name = classes_dict_lookup[predicted_id.argmax()]
+    
     if predicted_name in classes:
         return True, predicted_name
     else:
