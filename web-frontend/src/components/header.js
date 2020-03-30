8b338a3b484f15dc65b0a7754868be218ceaaf8f
Aditya Dhara <ocamlmycaml@github.com>
2020-03-30 19:02:34 -0400

    A few more web-frontend tweak

--- web-frontend/src/components/header.js
+++ web-frontend/src/components/header.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import ScarecrowLogo from "./images/scarecrow_logo"
+import "./header.css"
 
 const Header = ({ siteTitle }) => {
 
@@ -14,6 +15,7 @@ const Header = ({ siteTitle }) => {
   >
     <div
       style={{
+        display: `inline`,
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
@@ -24,6 +26,7 @@ const Header = ({ siteTitle }) => {
         style={{
           color: `white`,
           textDecoration: `none`,
+          display: `inline`
         }}
       >
         <ScarecrowLogo style={{ float: `left`, marginRight: `10px` }} />
@@ -35,6 +38,12 @@ const Header = ({ siteTitle }) => {
       <Link to="/members/">Members</Link>
       <Link to="/how_it_works/">How It Works</Link>
       <Link to="/farm_report/">Farm Report</Link>
+      {/* <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/members/">Members</Link></li>
+        <li><Link to="/how_it_works/">How It Works</Link></li>
+        <li><Link to="/farm_report/">Farm Report</Link></li>
+      </ul> */}
     </div>
   </header>
 }
